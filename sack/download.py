c6263dfa61604ed381a335985a9da9135c4a53ff
Karol Pasternak <karol@pasternak.pro>
2015-09-23 19:50:24 +0100

    zip archive manipulation added

--- sack/download.py
+++ sack/download.py
@@ -5,10 +5,14 @@ import sys
 import os
 import urllib.request
 import tarfile
+import time
+from zipfile import ZipFile
 from urllib.parse import urljoin
 from prep import SearchForPackage
 from pretty import ProgressBar, Color
 from pkg_resources import parse_version
+from multiprocessing import Process as Task, Queue, Manager, Array
+from collections import OrderedDict
 
 PYPI_ENDPOINT = "https://pypi.python.org/simple/"
 
@@ -20,10 +24,12 @@ class DownloadPackage(object):
             Download Package class with version check logic
             -
             @pkg - package name to download
+            @package - used for OrderedDict and ProgressBar
             @quiet - do not print out any output
             @dependencies - indicator if current downloaded package is a dep
         """
         self.pkg = pkg
+        self.package = pkg
         self.quiet = quiet
         self.dependencies = dependencies
 
@@ -51,9 +57,17 @@ class DownloadPackage(object):
     def __unpack(self, members):
         pkg = re.findall("\w+", self.pkg)[0]
         for info in members:
-            # print("/".join(info.name.split("/")[1:]))
-            # print(self.pkg)
-            if "/".join(info.name.split("/")[1:]) in [
+            # unpack for both zip and tarfile
+            # ZipFile.namelist() returns list of file
+            # Tarfile.open returns class with attribute name
+            # so for zip: info
+            # for tar: info.name points to the same string
+            if not hasattr(info, "name"):
+                a_file = info             # ZipFile
+            else:
+                a_file = info.name      # TarFile
+
+            if "/".join(a_file.split("/")[1:]) in [
                 "{}.egg-info/requires.txt".format(pkg),
                 "requirements.txt"
             ]:
@@ -66,18 +80,37 @@ class DownloadPackage(object):
              or
              requirements.txt
         """
-        tar = tarfile.open("repo/{}".format(archive))
-        unpack = self.__unpack(tar)
+        if archive.endswith(".zip"):
+            arch = ZipFile("repo/{}".format(archive)).namelist()
+        else:
+            arch = tarfile.open("repo/{}".format(archive))
+        unpack = self.__unpack(arch)
+
+        # Check if there is no dependencies. If none, return None
         if unpack is None:
             return
-        a_file = tar.extractfile(member=self.__unpack(tar))
+
+        if not archive.endswith(".zip"):
+            a_file = arch.extractfile(member=unpack)
+        else:
+            zip_file = ZipFile("repo/{}".format(archive))
+            # Remove [python>= version] version checks
+            a_file = zip_file.read(unpack)
+            a_file = re.sub('\[.*\]', '', a_file.decode('ascii')).split()
         for dep in a_file:
-            dep = dep.decode('ascii')
+            if hasattr(dep, 'decode'):
+                dep = dep.decode('ascii')
             if re.match("^\w", dep):
                 download = DownloadPackage(dep, quiet=True, dependencies=True)
                 download()
 
-        tar.close()
+        # arch.close()
+
+    def hook(self, count, chunk, total):
+        downloaded = int(count * chunk)
+        if downloaded > total:
+            downloaded = total
+        progress[self.package] = (downloaded, total)
 
     def __download(self):
         package = SearchForPackage(PYPI_ENDPOINT)
@@ -86,28 +119,26 @@ class DownloadPackage(object):
                 package(re.findall("\w+", self.pkg)[0]):
 
             if self.version_check(self.pkg, package):
+                self.package = package
                 link = urljoin(PYPI_ENDPOINT, link, False)
                 link = link.replace("../", "")
                 if self.dependencies:
                     info = "  Downloading dependency: {}".format(
-                        self.pkg.split()[0], **Color)
+                        package, **Color)
                     ProgressBar.set_tab = 0
 
                 else:
                     info = "Requested: {} | Downloading: {}".format(
                         self.pkg.split()[0], package, **Color)
                     ProgressBar.set_tab = 0
-
+                info = info.ljust(50)
                 # print(info)
                 ProgressBar.text = info
                 d_file = "repo/{}.{}".format(package, extension)
                 if os.path.exists(d_file) is False:
                     urllib.request.urlretrieve(link, d_file,
-                                               reporthook=ProgressBar.hook)
-                    print()
-                else:
-                    print("{Red}File {} exists...ignoring".format(d_file,
-                                                                  **Color))
+                                               reporthook=self.hook)
+
                 urllib.request.urlcleanup()
                 self.dependencies_check("{}.{}".format(package, extension))
                 return True
@@ -120,5 +151,7 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Provide arg!")
         sys.exit(1)
+    status = Queue()
+    progress = OrderedDict()
     download = DownloadPackage(sys.argv[1])
     download()
